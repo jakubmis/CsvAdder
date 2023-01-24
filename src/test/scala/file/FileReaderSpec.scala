1c0710198b82761b338979b409281e4b0abcc903
jakubmis <jakub.mis@playbookengineering.com>
2023-01-24 22:08:50 +0100

    additional tests

--- src/test/scala/file/FileReaderSpec.scala
+++ src/test/scala/file/FileReaderSpec.scala
@@ -8,11 +8,21 @@ import org.scalatest.freespec.AsyncFreeSpec
 class FileReaderSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers {
 
   "FileReader " - {
-    "read file and sum corresponding groups" in {
+    "read numbers file and sum corresponding groups" in {
       FileReader[IO](5)
         .flatMap(_.fileStream("src/test/resources/numbers.csv"))
         .flatMap(_.compile.toVector).asserting {
         vector => vector shouldBe Vector((0, 15), (1, 7), (2, 9), (3, 11), (4, 13))
+      }
+    }
+  }
+
+  "FileReader " - {
+    "read numbers2.csv file and sum corresponding groups" in {
+      FileReader[IO](5)
+        .flatMap(_.fileStream("src/test/resources/numbers2.csv"))
+        .flatMap(_.compile.toVector).asserting {
+        vector => vector shouldBe Vector((0, 50), (1, 7), (2, 9), (3, 14), (4, 13))
       }
     }
   }
